9b4fd76436f341c1bffbcd6ce0f6445d4a14cccd
2022000000081-Haider <2022000000081@seu.edu.bd>
2025-09-23 00:40:51 +0600

    Haider Update Kappa_Richardson-Trapezoidal.c

--- Kappa_Richardson-Trapezoidal.c
+++ Kappa_Richardson-Trapezoidal.c
@@ -11,4 +11,17 @@ double trapezoidal(double a, double b, int n) {
     double h = (b - a) / n;
     double sum = 0.5 * (f(a) + f(b));
 
+   for (int i = 1; i < n; i++) {
+        sum += f(a + i * h);
+    }
+    return h * sum;
+}
+
+// Richardson Extrapolation applied to Trapezoidal Rule
+double richardson(double a, double b, int n) {
+    double T_h = trapezoidal(a, b, n);       // step h
+    double T_h2 = trapezoidal(a, b, 2 * n);  // step h/2
+    return (4 * T_h2 - T_h) / 3.0;
+}
+
 
