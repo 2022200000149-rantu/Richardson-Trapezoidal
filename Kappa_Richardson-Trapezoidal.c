f0e93811e8c5e31e0c1ebb20f91b9d591855ff04
2022200000149-rantu <2022200000149@seu.edu.bd>
2025-09-23 00:38:34 +0600

    RANTU Update Kappa_Richardson-Trapezoidal.c

--- Kappa_Richardson-Trapezoidal.c
+++ Kappa_Richardson-Trapezoidal.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+// Function to integrate: f(x) = e^x
+double f(double x) {
+    return exp(x);
+}
+
+// Trapezoidal Rule implementation
+double trapezoidal(double a, double b, int n) {
+    double h = (b - a) / n;
+    double sum = 0.5 * (f(a) + f(b));
+
 
